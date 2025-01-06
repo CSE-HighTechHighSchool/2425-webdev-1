c2f51c2b4d4a569f2e4ea1946d46956c840089a8
hummingbee57 <bhasinmaya01@gmail.com>
2025-01-06 13:40:58 -0500

    Import firebase config

---
+++ js/register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyDrkDGdwaN0Ny08t08JPv8mb3_jZqUsSRg",
+authDomain: "humming-bee-bakes.firebaseapp.com",
+projectId: "humming-bee-bakes",
+storageBucket: "humming-bee-bakes.firebasestorage.app",
+messagingSenderId: "626309569524",
+appId: "1:626309569524:web:45a224c3f6ddf8ea76f7a1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
