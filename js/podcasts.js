69313ea9d066d393d3cedaeaaac80a0e10cc941f
mahuan16 <mariahuan2008@gmail.com>
2025-01-06 13:07:43 -0500

    made it so stars can lock

---
+++ js/podcasts.js
@@ -0,0 +1,14 @@
+function checkInput(){
+    let star1 = document.getElementById("star1");
+    let star2 = document.getElementById("star2");
+    let star3 = document.getElementById("star3");
+    let star4 = document.getElementById("star4");
+    let star5 = document.getElementById("star5");
+
+    star1.setAttribute('disabled', '');
+    star2.setAttribute('disabled', '');
+    star3.setAttribute('disabled', '');
+    star4.setAttribute('disabled', '');
+    star5.setAttribute('disabled', '');
+    
+}
