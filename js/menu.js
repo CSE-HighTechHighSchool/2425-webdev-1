70c7914833e4c726ab7e6a9c3cdc2f202a0d7d0e
hummingbee57 <bhasinmaya01@gmail.com>
2025-01-13 01:25:48 -0500

    Fixed account buttons after merge

--- js/menu.js
+++ js/menu.js
@@ -54,6 +54,7 @@ function addItemToOrder(userID, item, quantity){
 //Onload functions
 window.onload = function(){
   getUserName();  //Grab user information
+  let userLink = document.getElementById("account-button")
 
   if(!currentUser){ //if logged out, hide order elements
     const orderElements = document.querySelectorAll(".order-select");
